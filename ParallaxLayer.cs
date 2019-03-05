b878e4b2a44652a108be0415ddcdd8baed3b356a
Mano <r0740200@student.thomasmore.be>
2019-03-05 10:58:41 +0100

    Adding Codes

---
+++ ParallaxLayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxLayer : MonoBehaviour
+{
+    public float parallaxFactor;
+    public void Move(float delta)
+    {
+        Vector3 newPos = transform.localPosition;
+        newPos.x -= delta * parallaxFactor;
+        transform.localPosition = newPos;
+    }
+}
+
+public class ParallaxCamera : MonoBehaviour
+{
+    public delegate void ParallaxCameraDelegate(float deltaMovement);
+    public ParallaxCameraDelegate onCameraTranslate;
+    private float oldPosition;
+    void Start()
+    {
+        oldPosition = transform.position.x;
+    }
+    void Update()
+    {
+        if (transform.position.x != oldPosition)
+        {
+            if (onCameraTranslate != null)
+            {
+                float delta = oldPosition - transform.position.x;
+                onCameraTranslate(delta);
+            }
+            oldPosition = transform.position.x;
+        }
+    }
+}
